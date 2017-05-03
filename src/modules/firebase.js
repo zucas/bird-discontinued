ed4c189c80b781f787ac5dfb3315e0002837a680
Davi Zucas <davi_zucas@hotmail.com>
2017-05-02 22:13:03 -0300

    Update firebase.js

--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -1,9 +1,6 @@
 import Firebase from 'firebase'
-const firebaseApp = Firebase.initializeApp({apiKey: 'AIzaSyBnN56ni-3PPrOBUcFL99tSSN6p058nEU0',
-  authDomain: 'bird-ff640.firebaseapp.com',
-  databaseURL: 'https://bird-ff640.firebaseio.com',
-  projectId: 'bird-ff640',
-  storageBucket: 'bird-ff640.appspot.com',
-  messagingSenderId: '336177415259'})
+const firebaseApp = Firebase.initializeApp({
+//Your API KEY HERE  
+})
 const db = firebaseApp.database()
 export default db
