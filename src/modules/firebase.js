34e803814a38afc7f7df5c5aee1e223b6daa3bb5
Davi <davi_zucas@hotmail.com>
2017-05-26 08:38:27 -0300

    Removed folder and files from repository

--- src/modules/firebase.js
+++
@@ -1,9 +0,0 @@
-import Firebase from 'firebase'
-const firebaseApp = Firebase.initializeApp({apiKey: 'AIzaSyBnN56ni-3PPrOBUcFL99tSSN6p058nEU0',
-  authDomain: 'bird-ff640.firebaseapp.com',
-  databaseURL: 'https://bird-ff640.firebaseio.com',
-  projectId: 'bird-ff640',
-  storageBucket: 'bird-ff640.appspot.com',
-  messagingSenderId: '336177415259'})
-const db = firebaseApp.database()
-export default db
